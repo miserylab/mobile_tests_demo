5c2c628cdefb2a08c43779188532276ab785f4bd
Olga Demina <miserylab.r6s@gmail.com>
2022-10-08 19:32:06 +0300

    init commit

---
+++ tests/conftest.py
@@ -0,0 +1,48 @@
+__author__ = 'miserylab'
+
+import os
+
+import pytest
+from dotenv import load_dotenv
+from appium import webdriver
+from selene.support.shared import browser
+
+from utils import attach
+
+
+@pytest.fixture(scope='session', autouse=True)
+def env():
+    load_dotenv()
+
+
+@pytest.fixture(scope='function')
+def setup_browser():
+    options = {
+        #Specify device and os_version for testing
+        "platformName": "android",
+        "platformVersion": "9.0",
+        "deviceName": "Google Pixel 3",
+
+        #Set URL of the application under test
+        "app": "bs://c700ce60cf13ae8ed97705a55b8e022f13c5827c",
+
+        #Set other BrowserStack capabilities
+        'bstack:options': {
+            "projectName": "First Python project",
+            "buildName": "browserstack-build-DEMO",
+            "sessionName": "BStack first_test"
+        }
+    }
+    '''Set your access credentials'''
+    userName = os.getenv('LOGIN')
+    accessKey = os.getenv('KEY')
+    '''Initialize the remote Webdriver using BrowserStack remote URL 
+    and options defined above'''
+    browser.config.driver = webdriver.Remote(
+        command_executor=f"https://{userName}:{accessKey}@hub-cloud.browserstack.com/wd/hub",
+        desired_capabilities=options
+    )
+    browser.config.timeout = 4
+    yield setup_browser
+    attach.add_video(browser)
+    browser.quit()
---
+++ tests/test_browserstack_sample_selene.py
@@ -0,0 +1,21 @@
+__author__ = 'miserylab'
+
+import allure
+from allure import step
+from appium.webdriver.common.appiumby import AppiumBy
+from selene import have
+from selene.support.shared import browser
+from utils.attach import add_video
+
+
+@allure.title('Wikipedia search BrowserStack')
+def test(setup_browser):
+    with step('Search for content'):
+        browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
+
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type("BrowserStack")
+
+    with step('Content should be found'):
+        browser.all((AppiumBy.CLASS_NAME, "android.widget.TextView")).should(have.size_greater_than(0))
+    add_video(browser)
+
